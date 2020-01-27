fcd3546ed1251ac0475cb7102e7af2f22362afaa
emmanuel agarry <emmanuelagarry@gmail.com>
2020-01-27 14:20:04 +0100

    demo end 1.1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,4 @@
 export const environment = {
   production: true,
-  firebase: {
-    apiKey: 'AIzaSyAxYhCo_zpJDq0rHJ0uo723P4U-Dy3dS8o',
-    authDomain: 'foodappjj-dev.firebaseapp.com',
-    databaseURL: 'https://foodappjj-dev.firebaseio.com',
-    projectId: 'foodappjj-dev',
-    storageBucket: 'foodappjj-dev.appspot.com',
-    messagingSenderId: '545328620955',
-    appId: '1:545328620955:web:55a9893ccbc74843d42d3e',
-  },
+  firebase: {},
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,15 +4,7 @@
 
 export const environment = {
   production: false,
-  firebase: {
-    apiKey: 'AIzaSyAxYhCo_zpJDq0rHJ0uo723P4U-Dy3dS8o',
-    authDomain: 'foodappjj-dev.firebaseapp.com',
-    databaseURL: 'https://foodappjj-dev.firebaseio.com',
-    projectId: 'foodappjj-dev',
-    storageBucket: 'foodappjj-dev.appspot.com',
-    messagingSenderId: '545328620955',
-    appId: '1:545328620955:web:55a9893ccbc74843d42d3e',
-  },
+  firebase: {},
 }
 
 /*
